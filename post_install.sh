9a9fd7bd26a2f4bdbd6ea477c05f12de1f0fdc4d
thanhtuanto12 <56129240+thanhtuanto12@users.noreply.github.com>
2025-03-06 11:54:08 +0700

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 PRODUCT='NAKIVO Backup & Replication'
-URL="https://d96i82q710b04.cloudfront.net/res/product/NAKIVO_Backup_Replication_v11.0.1.89945_Installer-TRIAL.sh"
-SHA256="341a81eeb7c7837cae5675697cea51edb063c0350901b8bc4cbfeb94ba855050"
+URL="http://10.10.17.100:8080/NAKIVO_Backup_Replication_v11.1.0.91174_Installer-TRIAL.sh"
+SHA256="21371494d8e2db16a96f7344050ba1da008adab415a34776cbc38e8ad379b332"
 
 PRODUCT_ROOT="/usr/local/nakivo"
 INSTALL="inst.sh"
